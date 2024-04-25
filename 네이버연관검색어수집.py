2f46e371926cff40388c94e793488a3f7c6db25d
KIMGOEUN <ak0150a01@gmail.com>
2024-04-25 12:28:55 +0900

    related_srch

---
+++ 네이버연관검색어수집.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+keyword = input("키워드입력 >>")
+code = requests.get(f"https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query={keyword}")
+soup = BeautifulSoup(code.text, "html.parser")
+title = soup.select("div.related_srch div.tit")
+for i in title:
+    print(i.text)
